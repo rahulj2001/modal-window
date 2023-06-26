192dd610d2a5894b3912021f8605cabbcab26690
Rahul Jaideep <rahul.jaideep@ibm.com>
2023-06-26 02:35:37 -0400

    added functionality for modal windows

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+function open_handler() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function close_handler() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < showModal.length; i++){
+    showModal[i].addEventListener('click', open_handler);
+}
+
+closeModal.addEventListener('click', close_handler);
+overlay.addEventListener('click', close_handler);
+
